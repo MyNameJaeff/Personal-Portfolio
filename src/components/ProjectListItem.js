8493541fd3b61e9b132ab84655834af77f344933
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2024-01-16 10:34:03 +0100

    Added a bunch of images

--- src/components/ProjectListItem.js
+++ src/components/ProjectListItem.js
@@ -7,19 +7,33 @@ function ProjectListItem(props) {
 
     function handleClick(e) {
         if(props.number !== 0) {
+            let projectImages = list[props.number - 1].images;
+            let imagesStr = "";
+            projectImages.forEach((image) => {
+                imagesStr += `
+                    <img src="/images/${image}" alt="${image}" class="projectImage">
+                `;
+            });
+
+
             const projectMonitor = document.querySelector(".projectText");
             projectMonitor.style.justifyContent = "start";
             projectMonitor.style.alignItems = "start";
 
             const project = document.querySelector(".project");
             project.style.width = "100%";
-            project.style.height = "100%";
+            project.style.height = "90%";
             project.style.padding = "5%";
+            project.style.overflow = "scroll";
             project.style.flexDirection = "column";
             project.innerHTML = `
             <a href="${list[props.number - 1].link}" target="_blank"><h3 class="projectTitle">${largeFirstLetter}</h3></a>
-            <p class="projectTextContent"> ${list[props.number - 1].text}
+            <p class="projectTextContent"> ${list[props.number - 1].text} </p>
+            <div class="projectImages">
+                ${imagesStr}
+            </div>
             `;
+
         }
     }
     if (props.number !== 0) {
